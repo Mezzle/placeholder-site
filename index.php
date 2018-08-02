d3a0d69498e3a89d20c63664af163581de6c0c73
Martin Meredith <martin.meredith@hystreet.co.uk>
2018-08-02 13:31:04 +0100

    Initial Commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Placeholder</title>
+  <link rel="stylesheet" href="styles/normalize.css">
+  <link href="https://fonts.googleapis.com/css?family=Walter+Turncoat" rel="stylesheet">
+  <link rel="stylesheet" href="styles/main.css">
+</head>
+<body>
+<div class="container">
+  <div class="left"><img src="images/mezzle_character.svg"></div>
+  <div class="right">
+      <div class="speechbubble"><h1>Hi there!</h1><p>I managed to mess up all my websites, so that's why you're seeing this message.  It'll be back, eventually.</p></div>
+  </div>
+</div>
+</body>
+</html>
